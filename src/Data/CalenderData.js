b042f2d7371d833609a56fb2aeb93779bfefdacc
ahmed <ahmedmuhammed78613@gmail.com>
2022-12-01 18:07:17 +0000

    updated calender data with jokes

--- src/Data/CalenderData.js
+++ src/Data/CalenderData.js
@@ -1,56 +1,129 @@
 const CalenderData = {
-	day1: { fact: "Christmas wasn't always on December 25", past: true },
+	day1: {
+		fact: "Christmas wasn't always on December 25",
+		joke: "What did the wise men say after they offered up their gifts of gold and frankincense? Wait, there's myrrh.",
+		past: true,
+	},
 	day2: {
 		fact: "The Dutch gave us the idea to leave cookies and milk",
+		joke: "What do you get when you cross a snowman with a vampire? Frostbite.",
 		past: false,
 	},
-	day3: { fact: "Hanging stockings started by accident", past: false },
-	day4: { fact: "Black Friday isn't the busiest shopping day", past: false },
-	day5: { fact: "Coca-Cola played a part in Santa's image", past: false },
-	day6: { fact: "Rudolph was a marketing ploy", past: false },
-	day7: { fact: "", past: false },
+	day3: {
+		fact: "Hanging stockings started by accident",
+		joke: "What does Santa suffer from if he gets stuck in a chimney? Claus-trophobia!",
+		past: false,
+	},
+	day4: {
+		fact: "Black Friday isn't the busiest shopping day",
+		joke: "What do you call Santa when he stops moving? Santa Pause.",
+		past: false,
+	},
+	day5: {
+		fact: "Coca-Cola played a part in Santa's image",
+		joke: "What do snowmen eat for dessert? Ice crispies.",
+		past: false,
+	},
+	day6: {
+		fact: "Rudolph was a marketing ploy",
+		joke: "How do you help someone who has lost their Christmas spirit? Nurse them back to elf.",
+		past: false,
+	},
+	day7: {
+		fact: "What do you call a reindeer ghost? Cari-boo!",
+		joke: "What do you call an elf that runs away from Santa's workshop? A rebel without a Claus!",
+		past: false,
+	},
 	day8: {
 		fact: "You can thank Prince Albert for your Christmas tree",
+		joke: "What is it called when a snowman has a temper tantrum? A meltdown.",
 		past: false,
 	},
-	day9: { fact: "The eight tiny reindeer have had lots of names", past: false },
-	day0: { fact: "Christmas wreaths are religious symbols", past: false },
+	day9: {
+		fact: "The eight tiny reindeer have had lots of names",
+		joke: "Why are elves such great motivational speakers? They have plenty of elf-confidence.",
+		past: false,
+	},
+	day0: {
+		fact: "Christmas wreaths are religious symbols",
+		joke: "Why do reindeer like Beyoncé so much? She sleighs.",
+		past: false,
+	},
 	day10: {
 		fact: "'Jingle Bells' was originally a Thanksgiving song",
+		joke: "What reindeer game do reindeer play at sleepovers? Truth or deer.",
 		past: false,
 	},
 	day11: {
 		fact: "Astronauts broadcast 'Jingle Bells' from space",
+		joke: "What did Santa say when he stepped into a big puddle? It must have reindeer.",
 		past: false,
 	},
-	day12: { fact: "'Silent Night' is the most recorded song", past: false },
-	day13: { fact: "Celebrating Christmas used to be illegal", past: false },
-	day14: { fact: "Settlers created the first American eggnog", past: false },
+	day12: {
+		fact: "'Silent Night' is the most recorded song",
+		joke: "",
+		past: false,
+	},
+	day13: {
+		fact: "Celebrating Christmas used to be illegal",
+		joke: "What does Rudolph want for Christmas? A Pony sleigh station.",
+		past: false,
+	},
+	day14: {
+		fact: "Settlers created the first American eggnog",
+		joke: "What is Santa's dog's name? Santa Paws!",
+		past: false,
+	},
 	day15: {
 		fact: "Christmas decorating sends nearly 15,000 people to the ER",
+		joke: "Where do Santa's reindeer stop for coffee? Star-bucks!",
 		past: false,
 	},
-	day16: { fact: "Santa has his own Canadian postal code", past: false },
-	day17: { fact: "Dry Christmas trees spark more than joy", past: false },
-	day18: { fact: "The term 'Xmas' dates back to the 1500s", past: false },
-	day19: { fact: "Americans spend nearly $1,000 on gifts", past: false },
+	day16: {
+		fact: "Santa has his own Canadian postal code",
+		joke: "What's every elf's favorite type of music? Wrap!",
+		past: false,
+	},
+	day17: {
+		fact: "Dry Christmas trees spark more than joy",
+		joke: "What's the absolute best Christmas present? A broken drum — you can’t beat it!",
+		past: false,
+	},
+	day18: {
+		fact: "The term 'Xmas' dates back to the 1500s",
+		joke: "What happens if you eat Christmas decorations? You get tinsel-it is.",
+		past: false,
+	},
+	day19: {
+		fact: "Americans spend nearly $1,000 on gifts",
+		joke: "What do Santa's elves learn in school? The elf-abet.",
+		past: false,
+	},
 	day21: {
 		fact: "In Japan, a traditional Christmas dinner is to go to KFC",
+		joke: "What do you call an obnoxious reindeer? Rude-olph.",
 		past: false,
 	},
 	day22: {
 		fact: "The song 'We wish you a merry Christmas' was originally used as a threat",
+		joke: "What do grapes sing at Christmas? 'Tis the season to be jelly.",
 		past: false,
 	},
 	day23: {
-		fact: "In Austria instead of getting coal if you’ve been bad, you get Krampus",
+		fact: "In Austria instead of getting coal if you've been bad, you get Krampus",
+		joke: "What's the difference between the Christmas alphabet and the ordinary alphabet? The Christmas alphabet has Noel.",
 		past: false,
 	},
 	day24: {
 		fact: "Queen Victoria sent the first official Christmas card",
+		joke: "What did the gingerbread man put on his bed? A cookie sheet!",
 		past: false,
 	},
-	day25: { fact: "Merry Christmas", past: false },
+	day25: {
+		fact: "Merry Christmas",
+		joke: "Who is never hungry at Christmas? The turkey—he's always stuffed.",
+		past: false,
+	},
 };
 
 export default CalenderData;
