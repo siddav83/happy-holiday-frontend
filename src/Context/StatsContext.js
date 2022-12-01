95733995376e1747d6ac1994a4e1fc1169c6637a
ahmed <ahmedmuhammed78613@gmail.com>
2022-12-01 17:43:09 +0000

    added useContext

---
+++ src/Context/StatsContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useState } from "react";
+
+export const StatsContext = createContext();
+
+export const StatsProvider = (props) => {
+	const [stats, setStats] = useState({
+		stats_id: "",
+		posts: [],
+		want: [],
+	});
+
+	return (
+		<StatsContext.Provider
+			value={{
+				stats,
+				setStats,
+			}}
+		>
+			{props.children}
+		</StatsContext.Provider>
+	);
+};
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./main.css";
 import App from "./App";
+import { UserProvider } from "./Context/UserContext";
+import { CalenderProvider } from "./Context/CalenderContext";
+import { StatsProvider } from "./Context/StatsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<UserProvider>
+			<CalenderProvider>
+				<StatsProvider>
+					<Router>
+						<App />
+					</Router>
+				</StatsProvider>
+			</CalenderProvider>
+		</UserProvider>
 	</React.StrictMode>
 );
