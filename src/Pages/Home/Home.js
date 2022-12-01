70ca242ef4e55a997b2a9fe431e0e837284a5c73
Ami <ami1996_tm@live.co.uk>
2022-12-01 18:00:39 +0000

    Added countdown to homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
+const msToDays = 1000 * 60 * 60 * 24
+
 const Home = () => {
-	return <div>Home</div>;
+	const [loggedIn, setLoggedIn] = useState(null)
+	const [nextEvent, setNextEvent] = useState({
+		name: 'Christmas',
+		date: new Date(2022, 11, 25)
+	})
+	const [counter, setCounter] = useState(null)
+	const counterLimit = 99
+	
+	function daysLeft(target){
+		const timeLeft = target.getTime() - new Date().getTime()
+		return Math.ceil(timeLeft / msToDays)
+	}
+
+	useEffect(() => {
+		setCounter(daysLeft(nextEvent.date))
+	}, [nextEvent])
+
+	return <div>
+		<h1>Happy Holidays!</h1>
+
+		{
+			counter < counterLimit && <>
+				<h2>{counter}</h2>
+				<p>days until {nextEvent.name}</p>
+			</>
+		}
+
+		{
+			!loggedIn && <button>Login or sign up</button>
+		}
+	</div>;
 };
 
 export default Home;
