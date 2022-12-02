8e6ccebed5da6853d8d4ea7fd1b5610077237ff9
ahmed <ahmedmuhammed78613@gmail.com>
2022-12-02 15:54:11 +0000

    added snowflakes to background

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import { Routes, Route } from "react-router-dom";
 import { Home, User, Tab, Holidays } from "./Pages";
 import { TabNav, CategoryCard, AddNav } from "./Components";
 import Background from "./Assets/images/background.webp";
+import Snowflake from "./Assets/images/snowflake.png";
+import "./app.css";
 function App() {
 	return (
 		<div className="App">
-			<img className="background" src={Background} alt="snowfall" />
+			{/* <img className="background" src={Background} alt="snowfall" /> */}
+			{/* Snowflakes START*/}
+			<img src={Snowflake} alt="snowflake" className="snowflake s-1" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-2" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-3" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-4" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-5" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-6" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-7" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-8" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-9" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-10" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-11" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-12" />
+			<img src={Snowflake} alt="snowflake" className="snowflake s-13" />
+
+			{/* Snowflakes END*/}
 			<Routes>
 				{/* Pages */}
 				<Route path="/" element={<Home />} />
