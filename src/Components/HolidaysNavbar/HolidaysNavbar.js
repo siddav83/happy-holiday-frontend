5fcd51e5a9c17af3c2fb472242608fb8b2363682
ahmed <ahmedmuhammed78613@gmail.com>
2022-12-02 15:25:24 +0000

    fixed holidays navbar routing

--- src/Components/HolidaysNavbar/HolidaysNavbar.js
+++ src/Components/HolidaysNavbar/HolidaysNavbar.js
@@ -37,16 +37,19 @@ const Navbar = () => {
 			{/* Swirly Pattern END */}
 
 			<NavLink to="/">
-				<i className="fa-solid fa-right-from-bracket"></i>
+				<i
+					className="fa-solid fa-right-from-bracket"
+					onClick={toggleToMain}
+				></i>
 			</NavLink>
 			<NavLink to="/community">
-				<i className="fa-solid fa-earth-europe"></i>
+				<i className="fa-solid fa-earth-europe" onClick={toggleToMain}></i>
 			</NavLink>
 			<NavLink to="/holidays" onClick={toggleToMain}>
 				<i className="fa-solid fa-clock"></i>
 			</NavLink>
 			<NavLink to="/friends">
-				<i className="fa-solid fa-user-group"></i>
+				<i className="fa-solid fa-user-group" onClick={toggleToMain}></i>
 			</NavLink>
 		</nav>
 	);
