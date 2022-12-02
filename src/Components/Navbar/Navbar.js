87ab8c982885e705ba899db406c92fa85f300bdb
ahmed <ahmedmuhammed78613@gmail.com>
2022-12-02 10:32:08 +0000

    added holidays navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./style.css";
+import friendsIcon from "../../Assets/images/friends-icon.png";
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className="holiday-navbar">
+			<NavLink to="/">
+				<i class="fa-solid fa-right-from-bracket"></i>
+			</NavLink>
+			<NavLink to="/community">
+				<i class="fa-solid fa-earth-europe"></i>
+			</NavLink>
+			<NavLink to="/holidays">
+				<i class="fa-solid fa-clock"></i>
+			</NavLink>
+			<NavLink to="/friends">
+				<i class="fa-solid fa-user-group"></i>
+			</NavLink>
+		</nav>
+	);
 };
 
 export default Navbar;
