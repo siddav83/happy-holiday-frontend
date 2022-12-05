e59d4214114ae588d4c9241fb493f3d4e1fe692f
ahmed <ahmedmuhammed78613@gmail.com>
2022-12-05 11:50:56 +0000

    updated friends page

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -3,13 +3,16 @@ import "./style.css";
 import AvatarData from "../../Data/AvatarData";
 
 const Profile = () => {
+	const openProfile = () => {
+		console.log("Open profile");
+	};
 	return (
 		<div className="profile">
 			<div className="profile-avatar">
 				<img src={AvatarData[1]} alt="avatar" />
 			</div>
 
-			<div className="profile-username">
+			<div className="profile-username" onClick={openProfile}>
 				<h3>Username</h3>
 				<img
 					src="https://cdn-icons-png.flaticon.com/512/892/892817.png"
