2e2ad3536d4feebeabef41338e5da640fb1264cf
ahmed <ahmedmuhammed78613@gmail.com>
2022-12-05 16:52:17 +0000

    .

--- src/Pages/Friends/Friends.js
+++ src/Pages/Friends/Friends.js
@@ -1,22 +1,10 @@
 import React, { useContext } from "react";
 import "./style.css";
-import { Profile, HolidaysNavbar } from "../../Components";
+import { Profile, HolidaysNavbar, AddFriend } from "../../Components";
 import { ToggleContext } from "../../Context/ToggleContext";
 
 const Friends = () => {
 	const { toggleAddFriend, setToggleAddFriend } = useContext(ToggleContext);
-
-	const addNewFriend = (e) => {
-		e.preventDefault();
-		// Run a Post request
-		console.log("Adding friends...");
-	};
-
-	const inviteFriend = (e) => {
-		e.preventDefault();
-		// Run a Post request
-		console.log("Inviting friends...");
-	};
 
 	return (
 		<div className="main-container friends-container">
@@ -44,32 +32,7 @@ const Friends = () => {
 				<Profile />
 				<Profile />
 			</div>
-			{toggleAddFriend && (
-				<div className="add-or-invite-container">
-					<div className="add-or-invite">
-						<h3>Add A Friend</h3>
-						<form onSubmit={addNewFriend}>
-							<i className="fa-solid fa-circle-xmark"></i>
-							<input
-								type="text"
-								id="username"
-								placeholder="Enter Username..."
-							/>
-							<input type="submit" value="Add" />
-						</form>
-
-						<h3>Invite</h3>
-						<form onSubmit={inviteFriend}>
-							<i
-								className="fa-solid fa-circle-xmark"
-								onClick={() => setToggleAddFriend(false)}
-							/>
-							<input type="text" id="email" placeholder="Enter Email..." />
-							<input type="submit" value="Invite" />
-						</form>
-					</div>
-				</div>
-			)}
+			{toggleAddFriend && <AddFriend />}
 
 			<HolidaysNavbar />
 		</div>
