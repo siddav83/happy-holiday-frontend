473039fb341517e9a4df2802a0b6da4e01446d66
ahmed <ahmedmuhammed78613@gmail.com>
2022-12-06 12:17:31 +0000

    connected frontend to backend for homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,23 +1,27 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { Countdown, Layout, Logo, Modal } from "../../Components";
 import "./style.css";
+import { UserContext } from "../../Context/UserContext";
 
 const msToDays = 1000 * 60 * 60 * 24;
 const getFormData = (form) => Object.fromEntries(new FormData(form).entries());
 
 const Home = () => {
+	const navigate = useNavigate();
+	const { userData, setUserData } = useContext(UserContext);
 	const [loggedIn, setLoggedIn] = useState(null);
 	const [nextEvent, setNextEvent] = useState({
 		name: "Christmas",
 		date: new Date(2022, 11, 25),
-		icon: '🎄'
+		icon: "🎄",
 	});
 	const [countdown, setCountdown] = useState(null);
 	const countdownLimit = 99;
 	const [displayModal, setDisplayModal] = useState(null);
-	const [showSideMenu, setShowSideMenu] = useState(false)
-	const [output, setOutput] = useState('')
+	const [showSideMenu, setShowSideMenu] = useState(false);
+	const [output, setOutput] = useState("");
 
 	function daysLeft(target) {
 		const timeLeft = target.getTime() - new Date().getTime();
@@ -37,17 +41,22 @@ const Home = () => {
 			.post("http://127.0.0.1:5000/login", formData)
 			.then((res) => {
 				console.log(res, "POST RESPONSE");
-				if (res.status === 200) {
+				if (res.status === 201) {
 					console.log("LOGIN AS", formData["email"]);
 					setLoggedIn(formData);
 					closeModal();
+					setUserData((prev) => {
+						return { ...prev, email: formData.email, id: res.data.id };
+					});
+					// Redirect
+					navigate("/holidays");
 				} else {
 					console.log("FAILED");
 				}
 			})
 			.catch((err) => {
-				console.error(err)
-				setOutput('Incorrect email or password')
+				console.error(err);
+				setOutput("Incorrect email or password");
 			});
 	}
 
@@ -62,7 +71,7 @@ const Home = () => {
 				console.log(res, "POST RESPONSE");
 				if (res.status === 201) {
 					console.log("REGISTERED");
-					closeModal()
+					closeModal();
 				} else {
 					console.log("FAILED");
 				}
@@ -81,31 +90,37 @@ const Home = () => {
 	}, [nextEvent]);
 
 	useEffect(() => {
-		console.log("UPDATE", loggedIn)
-	}, [loggedIn])
+		console.log("UPDATE", loggedIn);
+	}, [loggedIn]);
 
 	return (
 		// <div className="main-container Home">
 		<Layout>
-		<div className={`Home ${displayModal ? 'no-overflow' : ''}`}>
-			<header>
-				<Logo/>
-				{/* <span className='logo'>Happy Holidays!</span> */}
-				{/* <button title='For development purposes only' onClick={() => setShowSideMenu(val => !val)}><i className='menu-icon'></i></button> */}
-			</header>
+			<div className={`Home ${displayModal ? "no-overflow" : ""}`}>
+				<header>
+					<Logo />
+					{/* <span className='logo'>Happy Holidays!</span> */}
+					{/* <button title='For development purposes only' onClick={() => setShowSideMenu(val => !val)}><i className='menu-icon'></i></button> */}
+				</header>
 
-			<main>
-				<div className="heading">
-					<h2>Celebrate {nextEvent?.icon} <span className='eventName'>{nextEvent ? nextEvent.name : 'holidays'}</span> with friends and family!</h2>
-				</div>
-				<div className='card-default'>
-					<div className='countdown'>
-						<div className='counter'>{countdown}</div>
-						<div className='text'>days until {nextEvent.name}</div>
+				<main>
+					<div className="heading">
+						<h2>
+							Celebrate {nextEvent?.icon}{" "}
+							<span className="eventName">
+								{nextEvent ? nextEvent.name : "holidays"}
+							</span>{" "}
+							with friends and family!
+						</h2>
 					</div>
-				</div>
-				<Countdown/>
-				{/* <div className='card-default'>
+					<div className="card-default">
+						<div className="countdown">
+							<div className="counter">{countdown}</div>
+							<div className="text">days until {nextEvent.name}</div>
+						</div>
+					</div>
+					<Countdown />
+					{/* <div className='card-default'>
 					<h2>My Wishlist</h2>
 					<div className='list'>
 					{
@@ -113,34 +128,39 @@ const Home = () => {
 					}
 					</div>
 				</div> */}
-				<div className='card-default'>
-					<h2>Popular Gifts</h2>
-					<div className='list'>
-						{
-							new Array(6).fill().map((item, index) => <div key={index} className='placeholder'></div>)
-						}
+					<div className="card-default">
+						<h2>Popular Gifts</h2>
+						<div className="list">
+							{new Array(6).fill().map((item, index) => (
+								<div key={index} className="placeholder"></div>
+							))}
+						</div>
 					</div>
-				</div>
-				<div className='card-default'>
-					<h2>Community Posts</h2>
-					<div className='list'>
-						{
-							new Array(6).fill().map((item, index) => <div key={index} className='placeholder'></div>)
-						}
+					<div className="card-default">
+						<h2>Community Posts</h2>
+						<div className="list">
+							{new Array(6).fill().map((item, index) => (
+								<div key={index} className="placeholder"></div>
+							))}
+						</div>
 					</div>
+				</main>
+				<div className="navbar-default">
+					{loggedIn ? (
+						<p>
+							Not {loggedIn.email}?{" "}
+							<a href="#" onClick={logout}>
+								Log out
+							</a>
+						</p>
+					) : (
+						<button onClick={() => setDisplayModal("Login")}>
+							Login or sign up
+						</button>
+					)}
 				</div>
-			</main>
-			<div className='navbar-default'>
-				{
-					loggedIn ?
-					<p>Not {loggedIn.email}? <a href="#" onClick={logout}>Log out</a></p> :
-					<button onClick={() => setDisplayModal("Login")}>
-						Login or sign up
-					</button>
-				}
-			</div>
 
-			{/* <div className='navbar-default'>
+				{/* <div className='navbar-default'>
 				{
 					loggedIn ?
 					<div>
@@ -156,114 +176,112 @@ const Home = () => {
 				}
 			</div> */}
 
-			{displayModal === "Login" && (
-				<Modal show={true} close={closeModal}>
-					<h2>Login</h2>
-					<form onSubmit={submitLogin}>
-						<label>
-							Email{" "}
-							<input
-								type="email"
-								name="email"
-								placeholder="Email address"
-								required
-							></input>
-						</label>
-						<label>
-							Password{" "}
-							<input
-								type="password"
-								name="password"
-								placeholder="Password"
-								required
-							></input>
-						</label>
-						<input type="submit" value="Login"></input>
-					</form>
-					{
-						output && <p className='alert'>{output}</p>
-					}
-					<a
-						href="#"
-						onClick={(e) => {
-							e.preventDefault();
-							setDisplayModal("Register");
-						}}
-					>
-						Register
-					</a>
-				</Modal>
-			)}
+				{displayModal === "Login" && (
+					<Modal show={true} close={closeModal}>
+						<h2>Login</h2>
+						<form onSubmit={submitLogin}>
+							<label>
+								Email{" "}
+								<input
+									type="email"
+									name="email"
+									placeholder="Email address"
+									required
+								></input>
+							</label>
+							<label>
+								Password{" "}
+								<input
+									type="password"
+									name="password"
+									placeholder="Password"
+									required
+								></input>
+							</label>
+							<input type="submit" value="Login"></input>
+						</form>
+						{output && <p className="alert">{output}</p>}
+						<a
+							href="#"
+							onClick={(e) => {
+								e.preventDefault();
+								setDisplayModal("Register");
+							}}
+						>
+							Register
+						</a>
+					</Modal>
+				)}
 
-			{displayModal === "Register" && (
-				<Modal show={true} close={closeModal}>
-					<h2>Register</h2>
-					<form onSubmit={submitRegister}>
-						<label>
-							Email{" "}
-							<input
-								type="email"
-								name="email"
-								placeholder="Email address"
-								required
-							></input>
-						</label>
-						<label>
-							Username{" "}
-							<input
-								type="text"
-								name="username"
-								placeholder="Username"
-								required
-							></input>
-						</label>
-						<label>
-							Password{" "}
-							<input
-								type="password"
-								name="password1"
-								placeholder="Password"
-								required
-							></input>
-						</label>
-						<label>
-							Confirm password{" "}
-							<input
-								type="password"
-								name="password2"
-								placeholder="Confirm password"
-								required
-							></input>
-						</label>
-						<input type="submit" value="Register"></input>
-					</form>
-					<a
-						href="#"
-						onClick={(e) => {
-							e.preventDefault();
-							setDisplayModal("Login");
-						}}
-					>
-						Login
-					</a>
-				</Modal>
-			)}
+				{displayModal === "Register" && (
+					<Modal show={true} close={closeModal}>
+						<h2>Register</h2>
+						<form onSubmit={submitRegister}>
+							<label>
+								Email{" "}
+								<input
+									type="email"
+									name="email"
+									placeholder="Email address"
+									required
+								></input>
+							</label>
+							<label>
+								Username{" "}
+								<input
+									type="text"
+									name="username"
+									placeholder="Username"
+									required
+								></input>
+							</label>
+							<label>
+								Password{" "}
+								<input
+									type="password"
+									name="password1"
+									placeholder="Password"
+									required
+								></input>
+							</label>
+							<label>
+								Confirm password{" "}
+								<input
+									type="password"
+									name="password2"
+									placeholder="Confirm password"
+									required
+								></input>
+							</label>
+							<input type="submit" value="Register"></input>
+						</form>
+						<a
+							href="#"
+							onClick={(e) => {
+								e.preventDefault();
+								setDisplayModal("Login");
+							}}
+						>
+							Login
+						</a>
+					</Modal>
+				)}
 
-			{/* For dev purposes only */}
-			<div className={`sideMenu ${showSideMenu ? 'open' : ''}`}>
-				<a>{loggedIn ? 'Logout' : 'Login'}</a>
-				<a>Register</a>
+				{/* For dev purposes only */}
+				<div className={`sideMenu ${showSideMenu ? "open" : ""}`}>
+					<a>{loggedIn ? "Logout" : "Login"}</a>
+					<a>Register</a>
 
-				<div>
-					<h2>Pages</h2>
-					<a>Community</a>
-					<a>Friends</a>
-					<a>Holidays</a>
-					<a>Tab</a>
-					<a>User</a>
+					<div>
+						<h2>Pages</h2>
+						<a>Community</a>
+						<a>Friends</a>
+						<a>Holidays</a>
+						<a>Tab</a>
+						<a>User</a>
+					</div>
 				</div>
 			</div>
-		</div>
 		</Layout>
 	);
 };
