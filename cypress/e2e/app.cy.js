b2ab0cc8ef15e15fef533b89bfdada248b9bf380
ahmed <ahmedmuhammed78613@gmail.com>
2022-12-06 15:38:44 +0000

    added cypress & configured coverage

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,47 @@
+describe("empty spec", () => {
+	beforeEach(() => {
+		cy.viewport(480, 900);
+	});
+
+	it("Opens Homepage", () => {
+		cy.visit("http://localhost:3000/");
+	});
+
+	it("User Logs in", () => {
+		cy.get("button").click();
+
+		cy.get(":nth-child(1) > input").type("a@a.com");
+
+		cy.get("form > :nth-child(2) > input").type("123");
+
+		cy.get('[type="submit"]').click();
+	});
+
+	it("User Opens Calender", () => {
+		cy.get(".calender-btn").click();
+
+		// Clicks on a day
+
+		cy.get(".calender-container > :nth-child(2)").click();
+
+		cy.get(".show-day > button > .fa-solid").click();
+
+		// Back button
+
+		cy.get(".back-btn").click();
+
+		// Goes back to calender
+
+		cy.get(".calender-btn").click();
+
+		// Nav button to go to Holidays main page
+
+		cy.get(".fa-regular").click();
+	});
+
+	it("User Opens their profile", () => {
+		cy.get('[href="/friends"]').click();
+
+		cy.get(".main-container > :nth-child(2) > .profile-username").click();
+	});
+});
