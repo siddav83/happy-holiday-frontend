6f90e29d340e6a26593d3c49ccb113af644a3395
Ami <ami1996_tm@live.co.uk>
2022-12-07 16:39:35 +0000

    Styling

--- src/Pages/Events/index.js
+++ src/Pages/Events/index.js
@@ -1,9 +1,14 @@
+import { Content, Navbar, Shortcuts } from '../../Components'
 import './style.css'
 
 export default function Events(){
     return (
         <div>
-            Events Page
+            <Navbar></Navbar>
+            <Content>
+                <h1>My Events</h1>
+            </Content>
+            <Shortcuts/>
         </div>
     )
 }
